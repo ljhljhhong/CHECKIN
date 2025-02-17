e6ef43ee4d0cae7dd33f6742160da046fd34d568
김준식 <105399710+popcorn0125@users.noreply.github.com>
2025-02-17 16:42:48 +0900

    250217 김준식 최근출결현황 진행중

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -9,7 +9,7 @@ React.useEffect(() => {
 return () => {}
 }, [])
 return (
-<div className={'container'}>
+    <div className={'container'}>
         <div className={'wrap'}>
             <div className={'h2-wrap'}>
                 <h2 className={'h2-text'}>홈 대시보드</h2>
@@ -33,79 +33,70 @@ return (
                     </div>
                 </div>
             </div>
-            <div className={'div-5'}>
-                <div className={'div-6'}>
-                    <div className={'div-7'}>
-                        <div className={'div-8'}>
-                            <div className={'text---'}>
-                                총 학생 수
-                            </div>
-                        </div>
-                        <div className={'div-9'}>
-                            <div className={'text-128'}>
-                                128명
-                            </div>
+            <div className={'info-wrap'}>
+                <div className={'infobox bk-blue'}>
+                    <div className={'infobox-title'}>
+                        <div className={'infobox-text color-blue'}>
+                            총 학생 수
                         </div>
                     </div>
-                    <div className={'div-10'}>
-                        <div className={'div-11'}>
-                            <div className={'text---4'}>
-                                오늘 출석률
-                            </div>
-                        </div>
-                        <div className={'div-12'}>
-                            <div className={'text-953'}>
-                                95.3%
-                            </div>
-                        </div>
-                    </div>
-                    <div className={'div-13'}>
-                        <div className={'div-14'}>
-                            <div className={'text----1'}>
-                                이번 주 출석률
-                            </div>
-                        </div>
-                        <div className={'div-15'}>
-                            <div className={'text-928'}>
-                                92.8%
-                            </div>
-                        </div>
-                    </div>
-                    <div className={'div-16'}>
-                        <div className={'div-17'}>
-                            <div className={'text----2'}>
-                                이번 달 출석률
-                            </div>
-                        </div>
-                        <div className={'div-18'}>
-                            <div className={'text-915'}>
-                                91.5%
-                            </div>
+                    <div className={'infobox-content'}>
+                        <div className={'infobox-content-text'}>
+                            128명
                         </div>
                     </div>
                 </div>
-                <div className={'div-19'}>
-                    <div className={'h3'}>
-                        <div className={'text----3'}>
-                            최근 출결 현황
+                <div className={'infobox bk-green'}>
+                    <div className={'infobox-title'}>
+                        <div className={'infobox-text color-green'}>
+                            오늘 출석률
                         </div>
                     </div>
-                    <div className={'div-20'}>
-                        <div className={'div-21'}>
-                            <div className={'div-22'}>
-                            </div>
+                    <div className={'infobox-content'}>
+                        <div className={'infobox-content-text'}>
+                            95.3%
                         </div>
-                        <div className={'div-23'}>
-                            <div className={'text-95'}>
-                                95%
-                            </div>
+                    </div>
+                </div>
+                <div className={'infobox bk-purple'}>
+                    <div className={'infobox-title'}>
+                        <div className={'infobox-text color-purple'}>
+                            이번 주 출석률
+                        </div>
+                    </div>
+                    <div className={'infobox-content'}>
+                        <div className={'infobox-content-text'}>
+                            92.8%
+                        </div>
+                    </div>
+                </div>
+                <div className={'infobox bk-orange'}>
+                    <div className={'infobox-title '}>
+                        <div className={'infobox-text color-orange'}>
+                            이번 달 출석률
+                        </div>
+                    </div>
+                    <div className={'infobox-content'}>
+                        <div className={'infobox-content-text'}>
+                            91.5%
                         </div>
                     </div>
                 </div>
             </div>
+            <div className={'bar-wrap'}>
+                <div className={'bar-wrap-h3'}>
+                    <h3 className='bar-wrap-h3-text'>최근 출결 현황</h3>
+                </div>
+                <div className={'progress-wrap'}>
+                    <progress value={85} max={100}/>
+                    <div className={'progress-text'}>
+                        95%
+                    </div>
+                </div>
+            </div>
         </div>
-        
-</div>
+    </div>
+
 );
 };
 export default Root

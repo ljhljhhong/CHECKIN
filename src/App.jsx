ae2db60633c703c140e55ad0421749e4f033d160
junhwan <rlawnsghs322.naver.com>
2025-02-20 16:32:29 +0900

    준환환

--- src/App.jsx
+++ src/App.jsx
@@ -2,8 +2,10 @@ import React, { Component } from 'react';
 import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import Header from './components/Header/Header.jsx';
 import Home from './pages/Home/Home.jsx';
+import Attendance_Management from './pages/Attendance_Management/Attendance_Management.jsx';
 import Main from './components/Dashboard/Dashboard.jsx';
-import Product from './pages/Home/Home.jsx';
+// import Product from './pages/Home/Home.jsx';
+import Product from './pages/Attendance_Management/Attendance_Management.jsx';
 import NotFound from './pages/NotFound/NotFound.jsx';
 import './App.css';
 const App = () => {
@@ -21,7 +23,7 @@ const App = () => {
 					{/*<Route path="*" element={<NotFound />}></Route>
 				</Routes> */}
 				<div className="right">
-					<Home />
+					<Attendance_Management/>
 				</div>
 			</BrowserRouter>
 		</div>

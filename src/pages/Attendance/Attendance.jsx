7a72332c645c4f9ddfb1583dc1480bcf0801dc94
김준식 <105399710+popcorn0125@users.noreply.github.com>
2025-02-21 16:53:33 +0900

    250221 수정

--- src/pages/Attendance/Attendance.jsx
+++ src/pages/Attendance/Attendance.jsx
@@ -1,72 +1,103 @@
 import React from 'react';
-import "../../styles/Attendance.css";
+import styles from "../../styles/Attendance.module.css";
 import Search from '../../assets/icons/search.svg?react';
 
 const Attendance = () => {
     return (
-        <div className={'container'}>
-                <div className={'wrap'}>
-                    <div className={'h2-wrap'}>
-                        <h2 className={'h2-text'}>출석률 현황</h2>
-                    </div>
-                    <div className={'button-wrap'}>
-                        <button className={'select-button'}>오늘</button>
-                        <button className={'non-select-button'}>지시별</button>
-                        <button className={'non-select-button'}>수업과목별</button>
-                    </div>
-                    <div className={'selectbox-wrap'}>
-                        <select id={'search'}>
-                            <option value="search_condition">검색조건</option>
-                            <option value="">이름</option>
-                            <option value="">학과</option>
-                            <option value="">학년</option>
-                        </select>
-                        <div className={'input-wrap'}>
-                            <Search id="12:02916" className={'svg'}/>
-                            <div className={'input'}>
-                                <input type='text' className={'text---3'} placeholder='검색어를 입력하세요'/>
-                            </div>
-                        </div>
-                    </div>
-                    <div className={'table'}>
-                        <div className={'thead'}>
-                            <div className={'tr'}>
-                                <div className={'th'}>
-                                <div className={'department'}>이름</div>
-                                </div>
-                                <div className={'th'}>
-                                    <div className={'classnumber'}>학번</div>
-                                </div>
-                                <div class={'th'}>
-                                    <div className={'name'}>팀명</div>
-                                </div>
-                                <div className={'th'}>
-                                    <div className={'attendance'}>출석률</div>
-                                </div>
-                            </div>
-                        </div>
-                        <div className={'tbody'}>
-                            <div className={'tr'}>
-                                <div className={'td'}>
-                                <div className={'department'}>김준식</div>
-                                </div>
-                                <div className={'td'}>
-                                    <div className={'classnumber'}>19120566</div>
-                                </div>
-                                <div className={'td'}>
-                                    <div className={'name'}>세미콜론</div>
-                                </div>
-                                <div className={'td'}>
-                                <div className={'attendance-td'}>95%</div>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    
+        <div className={styles.container}>
+            <div className={styles.wrap}>
+                <div className={styles.h2wrap}>
+                    <h2 className={styles.h2Text}>출석률 현황</h2>
                 </div>
-        
-                
+                <div className={styles.buttonWrap}>
+                    <button className={styles.selectButton}>오늘</button>
+                    <button className={styles.nonSelectButton}>지시별</button>
+                    <button className={styles.nonSelectButton}>수업과목별</button>
+                </div>
+                <div className={styles.selectboxWrap}>
+                    <select id={'search'}>
+                        <option value="search_condition">검색조건</option>
+                        <option value="">이름</option>
+                        <option value="">학과</option>
+                        <option value="">학년</option>
+                    </select>
+                    <div className={styles.inputWrap}>
+                        <Search id="12:02916" className={styles.svg} />
+                        <div className={styles.input}>
+                            <input type='text' className={styles.text3} placeholder='검색어를 입력하세요' />
+                        </div>
+                    </div>
+                </div>
+                <div className={styles.table}>
+                    <div className={styles.thead}>
+                        <div className={styles.tr}>
+                            <div className={styles.th}>
+                                <div className={styles.department}>이름</div>
+                            </div>
+                            <div className={styles.th}>
+                                <div className={styles.classnumber}>학번</div>
+                            </div>
+                            <div className={styles.th}>
+                                <div className={styles.name}>팀명</div>
+                            </div>
+                            <div className={styles.th}>
+                                <div className={styles.attendance}>출석률</div>
+                            </div>
+                        </div>
+                    </div>
+                    <div className={styles.tbody}>
+                        <div className={styles.tr}>
+                            <div className={styles.td}>
+                                <div className={styles.department}>김준식</div>
+                            </div>
+                            <div className={styles.td}>
+                                <div className={styles.classnumber}>19120566</div>
+                            </div>
+                            <div className={styles.td}>
+                                <div className={styles.name}>세미콜론</div>
+                            </div>
+                            <div className={styles.td}>
+                                <div className={styles.attendanceTd}>95%</div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
             </div>
+
+            <div className={styles.rowWrap} >
+                <div className={styles.h2wrap}>
+                    <h2 className={styles.h2Text}>출결 현황</h2>
+                </div>
+                <div className={styles.infoWrap}>
+                    <div className={styles.infoBox}>
+                        <div className={styles.infoBoxTitle}>
+                            전체 수업
+                        </div>
+                        <div className={styles.infoBoxContent}>
+                            32회
+                        </div>
+                    </div>
+                    <div className={styles.infoBox}>
+                        <div className={styles.infoBoxTitle}>
+                            출석률
+                        </div>
+                        <div className={styles.infoBoxContent}>
+                            91.5%
+                        </div>
+                    </div>
+                    <div className={styles.infoBox}>
+                        <div className={styles.infoBoxTitle}>
+                            평균 참석
+                        </div>
+                        <div className={styles.infoBoxContent}>
+                            29회
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
     );
 };
 

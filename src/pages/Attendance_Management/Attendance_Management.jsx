2e1ca127e2f6ca5e3382d558cb3e871ee3b3f37f
junhwan <rlawnsghs322.naver.com>
2025-02-27 16:35:27 +0900

    25.227 김준환 테이블 css 수정, container background 수정, 결석 지각 출석 글자색 수정 완료

--- src/pages/Attendance_Management/Attendance_Management.jsx
+++ src/pages/Attendance_Management/Attendance_Management.jsx
@@ -245,9 +245,6 @@ const initialStudents = [
 ];
 
 const Root = () => {
-  const [dropdownActive, setDropdownActive] = useState(false);
-  const toggleDropdown = () => setDropdownActive(!dropdownActive);
-
   // 텍스트 검색 상태
   const [textSearchCondition, setTextSearchCondition] = useState("이름");
   const [textSearchTerm, setTextSearchTerm] = useState("");
@@ -283,13 +280,14 @@ const Root = () => {
 
         {/* 날짜 검색 영역, 텍스트 검색 영역  */}
         <div className="selectbox-wrap" style={{ gap: "10px" }}>
-          <input 
+          <input
             type="date"
             className="select"
             value={dateSearchTerm}
             onChange={(e) => setDateSearchTerm(e.target.value)}
           />
-          <select className="select"
+          <select
+            className="select"
             id="text-search-condition"
             value={textSearchCondition}
             onChange={(e) => setTextSearchCondition(e.target.value)}
@@ -297,11 +295,10 @@ const Root = () => {
             <option value="이름">이름</option>
             <option value="학번">학번</option>
           </select>
-          <div className="input-wrap">
+          <div className="input-wrap" style={{ width: "300px" }}>
             <div className="input">
               <input
                 type="text"
-
                 placeholder="검색어를 입력하세요"
                 value={textSearchTerm}
                 onChange={(e) => setTextSearchTerm(e.target.value)}
@@ -371,45 +368,13 @@ const Root = () => {
           </div>
         </div>
 
-        {/* 수업 시간표 / 전체 시간 드롭다운 */}
-        <div className="info-wrap-1" style={{ marginTop: "20px" }}>
+        {/* 수업 시간표 */}
+        <div
+          className="info-wrap-1"
+          style={{ marginTop: "20px", marginBottom: "20px" }}
+        >
           <div className="h3">
             <div className="text--">수업 시간표</div>
-          </div>
-          <div
-            className={`select ${dropdownActive ? "active" : ""}`}
-            onClick={toggleDropdown}
-            style={{ position: "relative" }}
-          >
-            <div className="text---1">전체 시간</div>
-            {dropdownActive && (
-              <div className="options">
-                <div className="option" onClick={() => {}}>
-                  옵션1
-                </div>
-                <div className="option" onClick={() => {}}>
-                  옵션2
-                </div>
-                <div className="option" onClick={() => {}}>
-                  옵션3
-                </div>
-                <div className="option" onClick={() => {}}>
-                  옵션4
-                </div>
-                <div className="option" onClick={() => {}}>
-                  옵션5
-                </div>
-                <div className="option" onClick={() => {}}>
-                  옵션6
-                </div>
-                <div className="option" onClick={() => {}}>
-                  옵션7
-                </div>
-                <div className="option" onClick={() => {}}>
-                  옵션8
-                </div>
-              </div>
-            )}
           </div>
         </div>
 
